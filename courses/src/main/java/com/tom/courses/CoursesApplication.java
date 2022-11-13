a875119dd8c4d86ed2998c88ec808317cf207567
tomszociepa <tom.ociepa@gmail.com>
2022-11-13 16:05:33 +0100

    connect courses with eureka

--- courses/src/main/java/com/tom/courses/CoursesApplication.java
+++ courses/src/main/java/com/tom/courses/CoursesApplication.java
@@ -2,8 +2,10 @@ package com.tom.courses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CoursesApplication {
 
 	public static void main(String[] args) {
